b8762b43e3f56e07acd855ce5e88ccf4fdec4c5e
Francisco Sancho <frsancho1985@gmail.com>
2026-04-26 21:36:00 -0300

    feat: add main.jsx entry point with all providers

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
+import theme from './theme/theme'
+import AnalyticsProvider from './components/analytics/AnalyticsProvider/AnalyticsProvider'
+import AppRoutes from './routes/AppRoutes'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <AnalyticsProvider>
+            <AppRoutes />
+          </AnalyticsProvider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </HelmetProvider>
+    <Analytics />
+  </React.StrictMode>
+)
